1a885a5c994a3ab22d96bf46b15fe4e76265dddb
petrugrosu24061977 <tab.grosu@gmail.com>
2019-08-29 15:24:56 +0200

    Candidate++

---
+++ TestOnline/TestOnLine.Business/CandidateBusiness.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestOnline.Common;
+
+namespace TestOnLine.Business
+{
+
+public  class CandidateRepository
+    {
+
+
+
+        public static IEnumerable<CandidateModel> Get()
+        {
+            return CandidateRepository.Get();
+        }
+
+
+
+        public static CandidateModel Get(int id)
+        {
+            return CandidateRepository.Get(id);
+        }
+
+
+        public static void Post(CandidateModel candidateModel)
+        {
+            CandidateRepository.Post(candidateModel);
+        }
+
+
+        public static void Put(int id, CandidateModel candidateModel)
+        {
+            CandidateRepository.Put(id, candidateModel);
+        }
+
+
+         public static void Delete(int id)
+        {
+            CandidateRepository.Delete(id);
+        }
+
+
+
+    }
+}
