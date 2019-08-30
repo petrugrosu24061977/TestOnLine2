4aff2ff51e31080a8a2b37daa37077b9ee016e47
petrugrosu24061977 <tab.grosu@gmail.com>
2019-08-30 18:53:03 +0200

    version .Net Core

--- TestOnline/TestOnLine.Common/CandidateModel.cs
+++ TestOnline/TestOnLine.Common/CandidateModel.cs
@@ -1,15 +1,15 @@
-
+﻿
 namespace TestOnline.Common
 {
     using System;
     using System.Collections.Generic;
-    
+
     public partial class CandidateModel
     {
-    
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-    
+
     }
 }
