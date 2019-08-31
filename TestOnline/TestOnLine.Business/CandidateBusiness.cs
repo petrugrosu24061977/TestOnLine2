262d6db49de5215db947d6c0917e5c13dfa3a6c1
petrugrosu24061977 <tab.grosu@gmail.com>
2019-08-31 16:28:57 +0200

    version avec EF

--- TestOnline/TestOnLine.Business/CandidateBusiness.cs
+++ TestOnline/TestOnLine.Business/CandidateBusiness.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using TestOnline.Common;
-using TestOnLine.DAL;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using TestOnline.Common;
+//using TestOnLine.DAL;
 
-namespace TestOnLine.Business
-{
+//namespace TestOnLine.Business
+//{
 
-    public class CandidateBusiness
-    {
-
-
-
-        public static IEnumerable<CandidateModel> Get()
-        {
-            return CandidateRepository.Get();
-        }
+//    public class CandidateBusiness
+//    {
 
 
 
-        public static CandidateModel Get(int id)
-        {
-            return CandidateRepository.Get(id);
-        }
-
-
-        public static void Post(CandidateModel candidateModel)
-        {
-            CandidateRepository.Post(candidateModel);
-        }
-
-
-        public static void Put(int id, CandidateModel candidateModel)
-        {
-            CandidateRepository.Put(id, candidateModel);
-        }
-
-
-        public static void Delete(int id)
-        {
-            CandidateRepository.Delete(id);
-        }
+//        public static IEnumerable<CandidateModel> Get()
+//        {
+//            return CandidateRepository.Get();
+//        }
 
 
 
-    }
-}
+//        public static CandidateModel Get(int id)
+//        {
+//            return CandidateRepository.Get(id);
+//        }
+
+
+//        public static void Post(CandidateModel candidateModel)
+//        {
+//            CandidateRepository.Post(candidateModel);
+//        }
+
+
+//        public static void Put(int id, CandidateModel candidateModel)
+//        {
+//            CandidateRepository.Put(id, candidateModel);
+//        }
+
+
+//        public static void Delete(int id)
+//        {
+//            CandidateRepository.Delete(id);
+//        }
+
+
+
+//    }
+//}
