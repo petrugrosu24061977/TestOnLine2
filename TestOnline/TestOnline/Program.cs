0925bd9a8d52c37c983d433896c92d8bea126447
Kevin Sousselier <ksousselier@fuwear.fwrc>
2019-09-02 20:52:18 +0200

    Test commit

--- TestOnline/TestOnline/Program.cs
+++ TestOnline/TestOnline/Program.cs
@@ -19,6 +19,6 @@ namespace TestOnline
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>(); 
     }
 }
